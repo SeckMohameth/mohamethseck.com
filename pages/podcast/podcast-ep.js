c23dc6eff6b3af9e36d5f9689999fe68f41bb3b8
Mohameth Seck <30297403+SeckMohameth@users.noreply.github.com>
2020-12-12 15:45:47 -0500

    added facebook podcast and MO++

--- pages/podcast/podcast-ep.js
+++ pages/podcast/podcast-ep.js
@@ -13,11 +13,18 @@ export default function Podcast() {
         <p>Here are some podcasts I've been a part of. I also have a podcast of my own called MO++</p>
         <br/>
 
+        <iframe src="https://open.spotify.com/embed-podcast/show/7vbLGoJePsVnvkRCOeZqiO" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
         <iframe src="https://open.spotify.com/embed-podcast/episode/0XbJ5VCxJ1IzcqE7F9MeS4" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 
         <iframe src="https://open.spotify.com/embed-podcast/episode/2PuyrwXogA1iQ7MF3iU4Vz" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 
-        <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FNewHavenIndependent%2Fvideos%2F2428012144143501%2F&show_text=false&width=560" width="560" height="314" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>    </Layout>
+
+        <h3>Link to video of the podcast on <a href="https://fb.watch/2kVkLlwPlP/" target="_blank">FaceBook</a></h3>
+        <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/700159090&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/new-haven-independent" title="WNHH Community Radio" target="_blank" style="color: #cccccc; text-decoration: none;">WNHH Community Radio</a> · <a href="https://soundcloud.com/new-haven-independent/nhv-innovation-podcast-hoberton-school" title="NHV Innovation Podcast | Hoberton School" target="_blank" style="color: #cccccc; text-decoration: none;">NHV Innovation Podcast | Hoberton School</a></div>  
+
+
+        </Layout>
         </>
     )
 }
