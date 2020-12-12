6c7d75a431044516be896694a94ef674a2a0864f
Mohameth Seck <30297403+SeckMohameth@users.noreply.github.com>
2020-12-12 15:52:49 -0500

    added react-player npm to embed soundcloud podcast

--- pages/podcast/podcast-ep.js
+++ pages/podcast/podcast-ep.js
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 import Head from 'next/head'
 import Layout from '../../components/layout'
+import ReactPlayer from "react-player"
+
 
 export default function Podcast() {
     return(
@@ -21,7 +23,7 @@ export default function Podcast() {
 
 
         <h3>Link to video of the podcast on <a href="https://fb.watch/2kVkLlwPlP/" target="_blank">FaceBook</a></h3>
-        <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/700159090&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/new-haven-independent" title="WNHH Community Radio" target="_blank" style="color: #cccccc; text-decoration: none;">WNHH Community Radio</a> · <a href="https://soundcloud.com/new-haven-independent/nhv-innovation-podcast-hoberton-school" title="NHV Innovation Podcast | Hoberton School" target="_blank" style="color: #cccccc; text-decoration: none;">NHV Innovation Podcast | Hoberton School</a></div>
+        <ReactPlayer url="https://soundcloud.com/new-haven-independent/nhv-innovation-podcast-hoberton-school?in=new-haven-independent/sets/nhv-innovative-podcast"/>
 
         </Layout>
         </>
