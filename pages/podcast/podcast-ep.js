c351bbd8cd9788b800b4b9f34d4455cb86411a35
Mohameth Seck <30297403+SeckMohameth@users.noreply.github.com>
2020-12-12 15:36:10 -0500

    error with iframe

--- pages/podcast/podcast-ep.js
+++ pages/podcast/podcast-ep.js
@@ -13,11 +13,11 @@ export default function Podcast() {
         <p>Here are some podcasts I've been a part of. I also have a podcast of my own called MO++</p>
         <br/>
 
-        <iframe src="https://open.spotify.com/embed-podcast/episode/0XbJ5VCxJ1IzcqE7F9MeS4" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        {/* <iframe src="https://open.spotify.com/embed-podcast/episode/0XbJ5VCxJ1IzcqE7F9MeS4" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 
         <iframe src="https://open.spotify.com/embed-podcast/episode/2PuyrwXogA1iQ7MF3iU4Vz" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 
-        <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FNewHavenIndependent%2Fvideos%2F2428012144143501%2F&show_text=true&width=560" width="560" height="429" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+        <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FNewHavenIndependent%2Fvideos%2F2428012144143501%2F&show_text=true&width=560" width="560" height="429" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe> */}
     </Layout>
         </>
     )
